0532286f75aa78bc3e647d750280b768186d7cb5
Ciaran Murphy <ciaranrmurphy@gmail.com>
2017-10-09 22:50:53 +0200

    Implement TODOs in train_feature_extraction

--- feature_extraction.py
+++ feature_extraction.py
@@ -19,7 +19,9 @@ fc7 = AlexNet(resized, feature_extract=True)
 # HINT: Look at the final layer definition in alexnet.py to get an idea of what this
 # should look like.
 shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
-probs = ...
+weights = tf.Variable(tf.truncated_normal(shape, mean = 0, stddev = 0.1))
+bias = tf.Variable(tf.zeros(shape[-1]))
+probs = tf.nn.softmax(tf.matmul(fc7, weights) + bias)
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
